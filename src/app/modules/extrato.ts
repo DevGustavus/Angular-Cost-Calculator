a53e2169b39fc8fbdf80d6461443c1757e833dfc
DevGustavus <gustavusinvictus@gmail.com>
2024-02-13 21:41:13 -0300

    add http request

---
+++ src/app/modules/extrato.ts
@@ -0,0 +1,6 @@
+export interface Extrato {
+    id?: number;
+    nome: string;
+    descricao: string;
+    valor: number;
+}
---
+++ src/app/services/extrato.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Extrato } from '../modules/extrato';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExtratoService {
+
+  private url: string = environment.api;
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  obterExtratos() {
+    return this.httpClient.get<Extrato[]>(this.url);
+  }
+
+}
